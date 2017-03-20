0377d9ba82603e3f3f28e90d719b555d7d14e728
haoziiy <136842490@qq.com>
2017-03-20 23:30:53 +0800

    imagedb

--- haostagram/models.py
+++ haostagram/models.py
@@ -1,13 +1,26 @@
 # -*- encoding=UTF-8 -*-
-
+from datetime import datetime
 from haostagram import db
 import random
+
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    url = db.Column(db.String(512))
+    created_date = db.Column(db.DateTime)
+    def __init__(self, url, user_id):
+        self.url = url
+        self.user_id = user_id
+        self.created_date = datetime.now()
+    def __repr__(self):
+        return '<Image %d %s>' % (self.id, self.url)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(80), unique=True)
     password = db.Column(db.String(32))
     head_url = db.Column(db.String(256))
+    images = db.relationship('Image', backref='user', lazy='dynamic')
 
     def __init__(self, username,password):
         self.username = username
--- manager.py
+++ manager.py
@@ -1,15 +1,22 @@
 # -*- encoding=UTF-8 -*-
 from haostagram import app, db
 from flask_script import Manager
-from haostagram.models import  User
+from haostagram.models import  User, Image
+import  random
+
 manager = Manager(app)
+
+def get_image_url():
+    return 'http://images.nowcoder.com/head/' + str(random.randint(0, 1000)) + 'm.png'
 
 @manager.command
 def init_database():
     db.drop_all()
     db.create_all()
-    for i in range(0,100):
+    for i in range(0, 100):
         db.session.add(User('User' + str(i), 'a' + str(i)))
+        for j in range(0, 3):  # 每人发三张图
+            db.session.add(Image(get_image_url(), i + 1))
     db.session.commit()
 
 
