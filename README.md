a4bca8f30a01f30cb9465e2108c6fb869e8d930d
haoziiy <136842490@qq.com>
2017-03-20 23:39:20 +0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Photograph-Flask
 
-python web项目临摹某个打不开的外国图片网站。
+python web项目临摹"某个打不开的外国图片网站"。
 
 Python 2.7.10
 Flask 0.12
--- c2.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals  # 头部加上这句
-from flask import Flask, render_template, request, make_response, redirect, flash, get_flashed_messages
-import logging
-from logging.handlers import RotatingFileHandler
